20dcf02db679281f7b7c07143e5fb97d1a5a7a62
Sie Hao <siehao.0727@gmail.com>
2021-01-15 21:07:27 +0800

    First version of myFood

---
+++ app/src/main/java/com/example/myfood/extra/TopfoodExtra.java
@@ -0,0 +1,62 @@
+package com.example.myfood.extra;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+
+import com.example.myfood.R;
+import com.example.myfood.model.Topfood;
+
+import java.util.List;
+
+public class TopfoodExtra extends RecyclerView.Adapter<TopfoodExtra.TopfoodViewHolder>{
+
+    Context context;
+    List<Topfood> TopfoodList;
+
+    public TopfoodExtra(Context context, List<Topfood> topfoodList) {
+        this.context = context;
+        this.TopfoodList = topfoodList;
+    }
+
+    @NonNull
+    @Override
+    public TopfoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.top_food_row_item,parent,false);
+        return new TopfoodViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TopfoodViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return TopfoodList.size();
+    }
+
+    public static final class TopfoodViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView foodImage;
+        TextView price,name;
+
+
+        public TopfoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+            foodImage=itemView.findViewById(R.id.food_image);
+            name=itemView.findViewById(R.id.quiche);
+            price=itemView.findViewById(R.id.quiche_price);
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myfood/model/Topfood.java
@@ -0,0 +1,38 @@
+package com.example.myfood.model;
+
+public class Topfood {
+
+    String name;
+    String price;
+    Integer image_source;
+
+    public Topfood(String name, String price, Integer image_link) {
+        this.name = name;
+        this.price = price;
+        this.image_source = image_link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public Integer getImage_source() {
+        return image_source;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setImage_source(Integer image_source) {
+        this.image_source = image_source;
+    }
+}
